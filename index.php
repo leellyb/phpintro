4bac561d0f6ef823fb6aec07d2903a9bcdd06aca
leellyb <lilobee20@gmail.com>
2021-02-10 15:12:23 +0300

    assignment

---
+++ index.php
@@ -0,0 +1,152 @@
+<?php
+/*$math;
+$english;
+$kiswahili;
+$sum;
+$average;
+
+
+$math=50;
+$english=50;
+$kiswahili=40;
+
+
+$sum= $math+$english+$kiswahili;
+$average= $sum /3;
+
+if ($average>=60) {
+	echo "you got first division";
+
+
+		# code...
+	}
+	elseif ($average>=50) {
+		echo "you got second division";
+		# code...
+	}
+	elseif ($average>=35) {
+	echo "you got third place";
+
+	else ()
+		echo "failed";
+
+		# code...
+	}
+	
+
+// using switch 
+
+	$math;
+$english;
+$kiswahili;
+$sum;
+$average;
+
+
+$math=60;
+$english=50;
+$kiswahili=40;
+
+
+$sum= $math+$english+$kiswahili;
+$average= $sum /3;
+
+switch ($average) {
+	case $average>=60:
+	echo "1st division";
+		# code...
+		break;
+	
+	case $average>=50:
+	echo "2nd division";
+		# code...
+		break;
+	case $average>=35:
+	echo "3rd division";
+		break;
+
+	default:
+	echo "failed"; 	
+}
+$moviename '';
+$leadactor '';
+$duration '';
+$rate '';
+$genre '';
+
+$sum;
+$average;
+
+if (isset($_POST{'save'})) {
+	// users input
+	$moviename= $_POST{'movie'}
+	$leadactor= $_POST{'actor'}
+	$duration= $_POST{'duration'}
+	$rate= $_POST{'rate'}
+	$genre= $_POST{'genre'}
+	# code...
+}
+
+$sum=$moviename+$leadactor+$duration+$rate+$genre;
+$average=$sum /5;
+
+echo "<h1>."$average".</h1>";
+
+switch ($average) {
+	case $average>=60:
+	echo "1st division";
+		# code...
+		break;
+		case $average>=50:
+		echo "2nd division";
+		break;
+		case $average>=35:
+		echo "3rd division";
+			# code...
+			break;
+			default:
+			echo "failed";
+
+	
+	
+		# code...
+		break;
+}*/
+#6
+for ($x = 0; $x <= 5; $x++) {
+  echo "***** <br>";
+}
+echo "<br>";
+echo "<br>";
+
+
+#7
+$a=7;
+$b=10;
+$c=0;
+ 
+ $c=$a;
+ $a=$b;
+ $b=$a;
+
+ echo "a=$b";
+ echo "<br>";
+ echo "b=$c";
+ echo "<br>";
+ echo "<br>";
+
+
+#8
+ $a=7;
+ $b=10;
+
+ $a=$b;
+$b=$a-3;
+ echo "a=$a <br>";
+ echo "b=$b";
+ echo "<br>";
+ echo "<br>";
+
+echo strrev("Hello World");
+
+?>
